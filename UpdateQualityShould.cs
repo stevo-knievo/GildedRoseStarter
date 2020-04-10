9f98dde64168b1e8a4e9175a564fb6b3b2160040
Stefan Koch <stevo.knievo@gmail.com>
2020-04-09 20:03:21 -0700

    added test for "All items have a SellIn value which denotes the number of days we have to sell the item"

--- UpdateQualityShould.cs
+++ UpdateQualityShould.cs
@@ -12,15 +12,19 @@ namespace GildedRoseKata
         [Fact]
         public void DoNothingGivenSulfuras()
         {
-            var items = new List<Item> {
-                                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-
-            };
+            var items = new List<Item> {new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80}};
             var gildedRose = new GildedRose(items);
-            
             gildedRose.UpdateQuality();
-            
             Assert.Equal(80, items.First().Quality);
+        }
+
+        [Fact]
+        public void AllItemsDecreaseSellInValue()
+        {
+            var items = new List<Item> {new Item {Name = "Foo", SellIn = 10, Quality = 9}};
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+            Assert.Equal(9, items.First().SellIn);
         }
     }
 }
